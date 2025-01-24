a9f582d570eefd3e197e453fd8601fc82d60f753
Danielle M Barna <49793254+danibarna@users.noreply.github.com>
2025-01-24 10:35:52 +0100

    commit again

---
+++ .venv/Scripts/testtest_python.py
@@ -0,0 +1,19 @@
+#%%
+# this is the first comment
+spam = 1  # and this is the second comment
+          # ... and now a third!
+text = "# This is not a comment because it's inside quotes."
+
+spam
+
+a = 3
+
+b = 2
+
+a+b
+
+print("Hello, World!")
+
+
+
+# %%
